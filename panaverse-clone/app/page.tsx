3da16df03a09ef7d8d548f16870b3ce320d01001
Badar Abbas <raibadar37218@gmail.com>
2023-11-14 20:52:50 +0500

    Hey

---
+++ panaverse-clone/app/page.tsx
@@ -0,0 +1,42 @@
+const Home = () => {
+  return (
+    <div>
+      <header>
+        <h1>Panaverse Clone</h1>
+        <p>Hey everyone this is my first project of next js 14</p>
+        <article>Certified Web 3.0 and Metaverse Developer: A Nationwide Program in Karachi, Lahore, Islamabad, and Peshawar</article>
+      </header>
+
+      <section>
+          <h3>Youtube Video</h3>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/ZJUOux360xM?si=Jy3cWlk8-sqnTPXR" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" ></iframe>
+        </section>
+
+      <main>
+        <section>
+          <h2>Pnaverse Image</h2>
+          <img  width="560" height="200" src="https://www.panaverse.co/red-p-logo-text_dao_croped.png" alt="Panaverse"/>
+        </section>
+
+        
+
+         
+
+        <section>
+          <h2>Video from my Laptop</h2>
+<video width="560" height="200" src="123.mov" controls></video>       
+ </section>
+
+         <section>
+          <h2>Contact Us</h2>
+          0301-0123456         </section>
+      </main>
+
+      <footer>
+        <p>© 2023 Panaverse Clone</p>
+      </footer>
+    </div>
+  )
+}
+
+export default Home; 
