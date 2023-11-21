1fdc97dc525a74ab3033c10ee2d7273cdcbade5d
Badar Abbas <raibadar37218@gmail.com>
2023-11-21 21:14:02 +0500

    Adding COE file

--- web-clone/app/contact/page.tsx
+++ web-clone/app/contact/page.tsx
@@ -8,7 +8,7 @@ function contect(){
                 <div className="nav-links" id="navLinks">
                     <i className="fa fa-times"></i>
                     <ul>
-                        <li><a href="index.html">Home</a></li>
+                        <li><a href="Home">Home</a></li>
                         <li><a href="course">Course</a></li>
                         <li><a href="blog">Blog</a></li>
                         <li><a href="about">About</a></li>
